a9a05d88c2bad420f83f3ad4a01e41302b2e8867
JoshWillson <43349890+JoshWillson@users.noreply.github.com>
2018-10-19 11:57:39 +0100

    Add files via upload

--- pub quiz code.py
+++ pub quiz code.py
@@ -6,11 +6,10 @@ from bs4 import BeautifulSoup
 import lxml
 
 def getSub(tags, t_w):
-     rem = 0
      sub = []
      for det in range (0,len(tags)):
           if tags[det] == 'SUBJECT':
-               sub.append(t_w[det-rem])
+               sub.append(t_w[det])
      return(sub)
 
 def getSummary(soup):
@@ -19,9 +18,9 @@ def getSummary(soup):
             p = (styleCorrect[1]).getText()
             if p.count(".") < 2 and len(styleCorrect) >= 3:
                 p = (styleCorrect[2]).getText()
-            if p.count(".") < 2 and len(styleCorrect) >= 4:
+            elif p.count(".") < 2 and len(styleCorrect) >= 4:
                 p = (styleCorrect[3]).getText()
-            if p.count(".") < 2 and len(styleCorrect) >= 5:
+            elif p.count(".") < 2 and len(styleCorrect) >= 5:
                 p = (styleCorrect[4]).getText()
         except:
             try:
@@ -41,8 +40,61 @@ def searchWiki(t_w):
 
      source = requests.get(url).text
      soup = BeautifulSoup(source, 'lxml')
-   
-     print(getSummary(soup))
+
+     para =   getSummary(soup)
+     return para
+
+def validate_int(num):
+    try:
+        int(num)
+        return True
+    except:
+        return False
+
+def checkExists(para):
+    para = para.strip()
+    if para[:10] == 'The page "':
+        return False
+    if para[:15] == 'View (previous ':
+        return False
+    if para == 'There were no results matching the query.':
+        return False
+    if para[13:] == 'may refer to:':
+        return False
+    return True
+
+def changeFormat(para, sub):
+    count = 0
+    for choicesub in sub:
+        count += 1
+        print (count,': ',choicesub)
+    run_sub_menu = True
+    while run_sub_menu == True:
+        print('Is there a word here that you would best descirbe your search?')
+        prompt = 'Please input a choice from 1 to '+str(count)+'\nIf there is no word here that would work, enter "none"'
+        choice = input(prompt)
+        if validate_int(choice) == True and int(choice) >= 1 and int(choice) <= count:
+            run_sub_menu = False
+            to_search = [(sub[int(choice)-1])]
+            para2 = searchWiki(to_search)
+            print(para2)
+            if checkExists(para2) == False:
+                print('This page still cannot be found')
+            else:
+                print ('\n\n',para2)
+        elif choice.lower() == 'none':
+            run_sub_menu = False
+            sub2 = input('Enter the question subject')
+            sub2 = sub2.split(' ')
+            para2 = searchWiki(sub2)
+            print(para2)
+            if checkExists(para2) == False:
+                print('This page still cannot be found')
+            else:
+                print ('\n\n',para2)
+        else:
+            print('That was not a valid choice')
+
 
 def main():
 
@@ -52,10 +104,8 @@ def main():
           print ()
           text = input('Input a question with a Proper pronoun, queston word and question detail')
           length = len(text)
-          print(length)
           if text.endswith('?'):
                text = text[0:-1]
-          print(text)
 
           t = text.lower()
           t_w = t.split(' ')
@@ -68,7 +118,7 @@ def main():
                tagged = temp[1]
                if tagged == 'WRB' or tagged == 'WP':
                     tagged = 'Q_WORD'
-               elif tagged == 'NNP' or tagged == 'NN' or tagged == 'NNS' or tagged == 'JJ':
+               elif tagged == 'NNP' or tagged == 'NN' or tagged == 'NNS':  #This needs more work   i.e. countries dont work
                     tagged = 'SUBJECT'
                elif tagged == 'VB' or tagged == 'VBD' or tagged == 'VBG' or tagged == 'VBN' or tagged == 'VBP' or tagged == 'VBZ':
                     tagged = 'Q_DETAIL'
@@ -76,12 +126,25 @@ def main():
                     tagged = 'NONE'
                tags.append(tagged)
           sub = getSub(tags, t_w)
-          print('Original question :',t_w)
-          print('Question Subject :',sub)
-          searchWiki(sub)
-     
+          print('\nOriginal question :',t_w)
+          print('Question Subject :',sub, '\n')
+          para = searchWiki(sub)
+          if checkExists(para) == False:
+              print('This page cannot be found')
+              run_refine_menu = True
+              while run_refine_menu == True:
+                  choice = input('Do you want me to try and refine your search? (or try again?) [Enter "refine" or "again"]')
+                  if choice.lower() == 'refine':
+                      run_refine_menu = False
+                      changeFormat(para, sub)
+                  elif choice.lower() == 'again':
+                      run_refine_menu = False
 
-             
+                  else:
+                      print('That is not a valid choice')
+          else:
+              print (para)
+
 
 main()
 
@@ -120,4 +183,3 @@ WDT: WH-determiner
 WP: WH-pronoun
 WRB: Wh-adverb
 '''
-
